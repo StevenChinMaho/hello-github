4d61a3e2dfdbf0707210e2cb7bb81f1543859d6f
StevenChinMaho <503steven10@gmail.com>
2025-09-24 01:19:45 +0800

    arr test

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-sum = 0
+"""sum = 0
 L = list(range(1, 10, 2))
 print(L)
 
@@ -6,8 +6,16 @@ for i in L:
     sum += i
 
 print(sum)
-
+"""
 '''
 range(1, 10, 2)
 [1, 3, 5, 7, 9]
 '''
+
+a = [1, 2, 3]
+
+b = a[0]
+
+b = 4
+
+print(a[0], b)
